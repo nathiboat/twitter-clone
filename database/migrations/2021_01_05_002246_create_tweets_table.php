e16d11926a9e439422a2f2f3522032b49564ff1a
Nathi Taengnim <nathi.t@outlook.com>
2021-01-05 02:46:47 +0000

    update database and model

--- database/migrations/2021_01_05_002246_create_tweets_table.php
+++ database/migrations/2021_01_05_002246_create_tweets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowersTable extends Migration
+class CreateTweetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,13 @@ class CreateFollowersTable extends Migration
      */
     public function up()
     {
-        Schema::create('followers', function (Blueprint $table) {
+        Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id')->unsigned();
-            $table->bigInteger('following_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('body', 250);
             $table->timestamps();
 
-
-        });
-
-        Schema::table('followers', function(Blueprint $table) {
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('following_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -35,6 +30,6 @@ class CreateFollowersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('followers');
+        Schema::dropIfExists('tweets');
     }
 }
