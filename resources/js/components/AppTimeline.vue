f6d63d95f6ac9c4498e7ffa513941b589442d514
Nathi Taengnim <nathi.t@outlook.com>
2021-01-18 00:22:04 +0000

    add tweets components

---
+++ resources/js/components/AppTimeline.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <app-tweet v-for="tweet in tweets"
+            :key="tweet.id"
+            :tweet="tweet"
+
+
+        >
+
+        </app-tweet>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                tweets: []
+            }
+        },
+
+        methods: {
+            async getTWeets () {
+                let response = await axios.get('/api/timeline')
+
+                this.tweets = response.data.data
+            }
+        },
+
+        mounted () {
+            this.getTWeets()
+        }
+    }
+</script>
---
+++ resources/js/components/tweets/AppTweetUsername.vue
@@ -0,0 +1,14 @@
+<template>
+    <span class="text-gray-300 font-bold">{{ user.name }} <span class="text-blue-400">@{{ user.username }}</span></span>
+</template>
+
+<script>
+    export default {
+        props: {
+            user: {
+                require: true,
+                type: Object
+            }
+        }
+    }
+</script>
