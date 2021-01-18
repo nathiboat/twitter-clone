6cb55ebaeb008770b4268e9216af9ef86e1cf6fb
Nathi Taengnim <nathi.t@outlook.com>
2021-01-18 00:33:33 +0000

    add user in tweet

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Tweet extends Model
 {
     use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
